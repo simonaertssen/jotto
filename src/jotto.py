54af11fb5d60260df18b2c12543207452493c86b
Simon Aertssen <sa@ticra.com>
2022-10-18 11:47:52 +0200

    Removed testing with the new and improved encoding and testing

--- src/jotto.py
+++ src/jotto.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import os
-from string import ascii_lowercase
 from typing import TextIO
 
 
@@ -10,20 +9,7 @@ def iscandidate(word: str, wordlen: int = 5) -> bool:
     Idenitfy whether the word is a candidate for our solution.
     Assume the word is lowercase.
     """
-    return len(word) == wordlen and len(set(word)) == wordlen
-
-
-def remove_leading_bit(x: int) -> int:
-    """
-    Remove the leading 1 that encodes front trailing zeros.
-    This is done using a bitwise and with hex(2**26 - 1) = 0x3ffffff.
-    """
-    return x & 0x3ffffff
-
-
-def seen_before(a: int, b: int) -> bool:
-    """Compute the AND of a and b, considering they have a leading 1."""
-    return remove_leading_bit(a & b) != 0
+    return len(word) == wordlen and not {"a", "e", "i", "o", "u"}.isdisjoint(word)
 
 
 def encode(word: str) -> int:
@@ -31,14 +17,14 @@ def encode(word: str) -> int:
     Encode a word for fast comparisons: index which letters are being used.
     Assume the word is lowercase and consists of 5 letters.
     """
-    sortword: str = [ascii_lowercase.index(letter) for letter in sorted(word)]
-    bitword: str = '1' + '0' * sortword[0] + '1'  # Prepend with a one, otherwise we cannot store leading zeros
-    bitword += '0' * (sortword[1] - sortword[0] - 1) + '1'
-    bitword += '0' * (sortword[2] - sortword[1] - 1) + '1'
-    bitword += '0' * (sortword[3] - sortword[2] - 1) + '1'
-    bitword += '0' * (sortword[4] - sortword[3] - 1) + '1'
-    bitword += '0' * (26 - sortword[4] - 1)   # Pad with 0's
-    return int(bitword, base=2)
+    ret: int = 0
+    a: int = ord("a")
+    ret |= 1 << (ord(word[0]) - a)
+    ret |= 1 << (ord(word[1]) - a)
+    ret |= 1 << (ord(word[2]) - a)
+    ret |= 1 << (ord(word[3]) - a)
+    ret |= 1 << (ord(word[4]) - a)
+    return ret
 
 
 def solve() -> None:
@@ -47,15 +33,25 @@ def solve() -> None:
     and which use 25 different letters of the western alphabet.
     """
 
-    # Find the file and open it, start
+    # Find the file and open it, start building a database of words
     filename: str = os.path.join('data', 'words_alpha.txt')
     filestream: TextIO = open(filename, 'r', newline=None)
 
+    anagrams: dict[list] = {}
+    for word in filestream:
+        word = word.strip()
+        if not iscandidate(word):
+            continue
+
+        intword: int = encode(word)
+        if word not in anagrams:
+            anagrams[intword] = [word]
+        else:
+            anagrams[intword].append(word)
+
     # Initialise an empty set of solutions and start solving
     solutions: set = set()
-    letters_taken: int = 0
 
-    count = 0
     for word in filestream:
         word = word.strip()
 
@@ -63,19 +59,19 @@ def solve() -> None:
             continue
 
         bitword: int = encode(word)
+        # print(f"{bitword:b}")
+        # print(ascii_lowercase)
         # print(f'candidate {word} = {bitword:b}: {seen_before(bitword, letters_taken):b}')
         if not seen_before(bitword, letters_taken):
             # print("{:b}".format(seen_before(bitword, letters_taken)))
             # print("{:b}".format(bitword))
 
             solutions.add(word)
-            letters_taken |= bitword
-            print("letters_taken:    {:b}".format(letters_taken))
-            input()
+            # print(f"{bitword:b}")
+            # input()
 
     # Cleanup
     filestream.close()
-    print(count)
     print(solutions)
     print(len(solutions))
     assert len(solutions) == 538
--- tests/test_jotto.py
+++ tests/test_jotto.py
@@ -2,7 +2,7 @@
 import unittest
 from string import ascii_lowercase
 
-from src.jotto import encode, iscandidate, remove_leading_bit, seen_before
+from src.jotto import encode, iscandidate, remove_leading_bit
 
 
 class TestJottoHelperFunctions(unittest.TestCase):
@@ -20,13 +20,6 @@ class TestJottoHelperFunctions(unittest.TestCase):
         for word in self.words:
             self.assertTrue(iscandidate(word))
 
-    def test_remove_leading_bit(self) -> None:
-        """Test whether we can correctly remove the first bit."""
-        bitword: str = '100000000000101001000000000'
-        value: int = int(bitword, base=2)
-        result: int = remove_leading_bit(value)
-        self.assertEqual(result, int(bitword[1:], 2))
-
     def test_word_encoding(self) -> None:
         """Test the correct encoding of a word."""
         for word in self.words:
@@ -35,20 +28,6 @@ class TestJottoHelperFunctions(unittest.TestCase):
             self.assertIsInstance(bitword, int)
             self.assertEqual(bitword, encode(testword))
             self.assertEqual(len(f"{bitword:b}"), len(ascii_lowercase) + 1)
-
-    def test_seen_before(self) -> None:
-        """Test whether we have seen this value before."""
-        bitval1: int = int('101101101001011010010011001', 2)
-        bitval2: int = int('110010010110100101101100110', 2)
-        self.assertFalse(seen_before(bitval1, bitval2))
-        self.assertTrue(seen_before(bitval1, bitval1))
-        self.assertTrue(seen_before(bitval2, bitval2))
-
-        bitval1: int = int('100000001001011010010011001', 2)
-        bitval2: int = int('100000000110100101101100110', 2)
-        self.assertFalse(seen_before(bitval1, bitval2))
-        self.assertTrue(seen_before(bitval1, bitval1))
-        self.assertTrue(seen_before(bitval2, bitval2))
 
     def test_existing_solution(self) -> None:
         """Test an existing solution to see if it can be detected with our current tools."""
